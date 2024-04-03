c849c9b7b3bb789f41dc92a0ac1261264d9050ca
park <uyeongi@naver.com>
2024-04-03 13:07:01 +0900

    제발

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
-'''
-깃 이그노어(이그노어 자체는 깃에 올려도 됨)에 벤브 파일을 명시해서 벤브파일은 안올리는걸로 하고 - 벤브는 절대 올리기x
+from flask import Flask, request, render_template, redirect
+import os
+from flask_sqlalchemy import SQLAlchemy
 
-app.py, html은 각자 기능별로 구현 // 백엔드는 app.py에 한번에 구현
-각자 기능에 맞는 게시물, 댓글 브런치에서 풀리퀘스트 해서 메인으로 머지 하고
-메인에 올라갔으면 풀로 당겨오기
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
 
-저녁 7시에 머지하고 풀로 당겨오기
-그전에는 각자 브랜치에서 만들기 - 서로 풀이랑 푸시 자주하고 확인해주세요.
-'''
-'그렇군요' '좋습니다'
+db = SQLAlchemy(app)   
+
+# db 정보 
+class board(db.Model):   
+    id = db.Column(db.Integer, primary_key=True) 
+    user = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+
+class Writing(db.Model):   
+    id = db.Column(db.Integer, primary_key=True) 
+    category = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    detail = db.Column(db.String, nullable=False)
+
+class textarea(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String, nullable=False)
+    comment = db.Column(db.String, nullable=False)
+    comment_date = db.Column(db.String, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == 'POST':
+        # POST 요청으로부터 데이터를 가져옴
+        user = request.form['user']
+        title = request.form['title']
+        content = request.form['content']
+        
+        # 데이터베이스에 게시물 추가
+        new_post = Writing(user=user, title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        
+        # 게시물 작성 후 리다이렉트 asd
+        return redirect('/category/free')
+    else:
+        # GET 요청인 경우 작성 페이지 렌더링
+        return render_template('write.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
